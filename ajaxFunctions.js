790e69197ce5ffaad154bb4bef3f1a14bf753c25
Nancy Howard <Nhansbery@Noahs-Macbook-Air.local>
2017-06-01 14:37:53 -0700

    f

--- ajaxFunctions.js
+++ ajaxFunctions.js
@@ -1,16 +1,17 @@
 /**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
+ * Uses AJAX to query an internet data source for number facts
+ * @param {string} numberId The element id that has the number fact
  */
-function findZip(zipId) {
-    var zip = document.getElementById(zipId).value;
+function findFact() {
+    var numero = document.getElementById("number").value;
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             if(this.status === 200) {
-                displayPlace(this.responseText);
+                console.log("Successful Request!!")
+                displayFact(this.responseText);
             } else if (this.status === 404){
-                displayPlace('{ "country" : "none" }');
+                displayFact('{ "number" : "none" }');
             } else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
@@ -18,26 +19,25 @@ function findZip(zipId) {
 
         }
     };
-    var url = "http://api.fixer.io/latest" + zip;
+    var url = "http://numbersapi.com/" + numero + "/" + "math?JSON";
     httpRequest.open("GET", url, true);
     httpRequest.send();
+
 }
 
 /**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
+ * Displays the number fact
+ * @param {string} data JSON data representing place for given number
  */
-function displayPlace(data){
-    var place = JSON.parse(data);
-    if(place.country === "none") {
-        document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "No place matches that zip code."
+
+function displayFact(data){
+    var numfact = data;
+    if(numfact.text === "none") {
+        document.getElementById("output").className = "alert alert-warning";
+        document.getElementById("output").innerHTML = "No numbers match your entry."
     } else {
-        document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = place.places[0]["place name"] +
-        ", " +
-        place.places[0].state +
-        ", " +
-        place.country;
+        document.getElementById("output").className = "alert alert-success";
+        document.getElementById("output").innerHTML = document.write(data);
+
     }
 }
